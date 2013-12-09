3222da5a0e235a7b88daabadbb3e02f1b4386ff4
Kristine <screamingmunch@gmail.com>
2013-12-09 11:11:48 -0800

    initial commit: setup pg & secret_token saved to .env file && .env file added to gitignore

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FieldInspector::Application.config.session_store :cookie_store, key: '_field_inspector_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FieldInspector::Application.config.session_store :active_record_store
